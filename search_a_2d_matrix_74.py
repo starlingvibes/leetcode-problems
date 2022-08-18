0e1dc3c4f464f96c0f982fdaa9cc658866eee06c
starlingvibes <starlingroot@protonmail.com>
2022-08-18 22:03:36 +0100

    added optimized solution

--- search_a_2d_matrix_74.py
+++ search_a_2d_matrix_74.py
@@ -26,3 +26,20 @@ class Solution:
             if target in i:
                 return True
         return False
+
+
+# O(log(rc)) solution
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows, cols = len(matrix), len(matrix[0])
+        first, last = 0, rows * cols - 1
+
+        while first <= last:
+            mid = (first + last) // 2
+            if matrix[mid // cols][mid % cols] == target:
+                return True
+            if matrix[mid // cols][mid % cols] > target:
+                last = mid - 1
+            if matrix[mid // cols][mid % cols] < target:
+                first = mid + 1
+        return False
