d1946af1bdb20427537b9b36c1d32bfd6c4d9407
starlingvibes <starlingroot@protonmail.com>
2022-08-22 03:21:47 +0100

    232 - Implement Queue using Stacks

--- implement_queue_using_stacks_232.py
+++ implement_queue_using_stacks_232.py
@@ -58,6 +58,32 @@ class MyQueue:
         return False
 
 
+# Amortized O(1) solution
+
+class MyQueue:
+
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, x: int) -> None:
+        self.s1 += [x]
+
+    def pop(self) -> int:
+        if len(self.s2) == 0:
+            while len(self.s1) > 0:
+                self.s2 += [self.s1.pop()]
+        return self.s2.pop()
+
+    def peek(self) -> int:
+        if len(self.s2) == 0:
+            while len(self.s1) > 0:
+                self.s2 += [self.s1.pop()]
+        return self.s2[-1]
+
+    def empty(self) -> bool:
+        return len(self.s1) == 0 and len(self.s2) == 0
+
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
 # obj.push(x)
