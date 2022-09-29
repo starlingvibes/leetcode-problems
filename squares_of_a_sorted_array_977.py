e155b187276c63460b069a444934ab20b97cda76
starlingvibes <starlingroot@protonmail.com>
2022-09-29 09:26:06 +0100

    977 - Squares of a sorted array

---
+++ squares_of_a_sorted_array_977.py
@@ -0,0 +1,40 @@
+"""
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+
+Example 1:
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+
+Example 2:
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+ 
+
+Constraints:
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums is sorted in non-decreasing order.
+ 
+
+Follow up: Squaring each element and sorting the new array is very trivial, could you find an O(n) solution using a different approach?
+"""
+
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        result = deque()
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            if nums[l] ** 2 <= nums[r] ** 2:
+                result.appendleft(nums[r]**2)
+                r -= 1
+
+            else:
+                result.appendleft(nums[l]**2)
+                l += 1
+
+        return result
